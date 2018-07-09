e7d528f78cd372403563030d60637bd6eaa1b8b1
luigi-zuniga <luis.alberto.zuniga@outlook.com>
2018-07-09 09:17:11 -0400

    P1.1.0.0 PRL Modificacion nav y header

---
+++ shared/header.php
@@ -0,0 +1,9 @@
+    <!-- Header con texto introductorio -->
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Texto corto introductorio o de bienvenida. No más de trés reglones para no hacer demasiado largo el párrafo
+                    y mantener el balance entre info textual y visual.</div>
+            </div>
+        </div>
+    </header>
---
+++ shared/nav.php
@@ -0,0 +1,42 @@
+    <!-- Navegacion -->
+    <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container">
+            <!-- barra de menu agrupada para dispositivos moviles -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menú
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <img src="img/PRL-logotipo-color-lg.png" class="img-responsive">Recomendaciones
+                    <br/>de Lectura</a>
+            </div>
+
+            <!-- Enlaces y formulario -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <form class="navbar-form" role="search" action="/Portales/Views/Recomendaciones/recom_list_busq.php?">
+                        <input type="text" class="form-control" placeholder="¿Buscas algo en particular?" />
+                        <button id="btn-search" type="submit" class="btn btn-primary btn-sm">Buscar</button>
+                    </form>
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/Portales/">Inicio</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/Portales/Views/Recomendaciones/recomendaciones.php">Recomendaciones</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/Portales/Views/Comites/comite.php">Comités</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/Portales/Views/Contacto/contacto.php">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
